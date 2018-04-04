ba558a7684ab308137d585e9f1ea46a30e77e10e
sterrevangeest <sterrevangeest@hotmail.nl>
2018-04-04 16:02:57 +0200

    added functions for rendering matches, matches-detail, inbox and profile.

--- index.js
+++ index.js
@@ -3,47 +3,86 @@
 var express = require('express')
 
 express()
-  //.set('view engine', 'ejs')
-  //.set('views', 'views')
-  .use(express.static('static'))
-  .get('/', all)
-  //.get('/:index', get)
-  .listen(8000)
+    .set('view engine', 'ejs')
+    .set('views', 'views')
+    .use(express.static('static'))
+    .get('/', all)
+    .get('/inbox', inbox)
+    .get('/profile', profile)
+    .get('/:index', detail)
+
+    .listen(8000)
 
 
+function all(req, res) {
+    console.log("hello")
+    res.render('matches.ejs', {
+        title: 'matches',
+        data: data
+    })
+}
+
+function detail(req, res) {
+
+    var id = req.params.index
+    console.log(id)
+    var length = data.length
+    console.log(length)
+
+    if (id < length) {
+        res.statuscode = 200 // 200 OK
+        res.render('matches-detail.ejs', {
+            title: 'matches-in-detail',
+            data: data[id]
+        })
+    } else {
+      res.statuscode = 404 // 404 Not Found
+      // res.render('error.ejs', {
+      //     title: '404',
+      // })
+      res.write("werkt niet")
+      res.end()
+    }
+}
+
+function inbox(req, res) {
+    console.log("inbox")
+    res.render('inbox.ejs', {
+        title: 'inbox',
+        data: data
+    })
+}
+
+function profile(req, res) {
+    console.log("profile")
+    res.render('profile.ejs', {
+        title: 'profile',
+        data: data
+    })
+}
+
+
+// data
 var data = [
-  {
-    name: 'Daan',
-    age: '23 jaar',
-    place: 'Amsteram',
-    dateIdea: 'Cocktails drinken in Amsterdam, bij Tunes Bar en daarna zien we wel.',
-    category: 'romantisch'
+    {
+        name: 'Daan',
+        age: '23 jaar',
+        place: 'Amsteram',
+        dateIdea: 'Cocktails drinken in Amsterdam, bij Tunes Bar en daarna zien we wel.',
+        category: 'romantisch'
   },
-
-  {
-    name: 'Bram',
-    age: '21 jaar',
-    place: 'Haarlem',
-    dateIdea: 'Wandeling op het strand bij Wijk aan Zee',
-    category: 'romantisch'
+    {
+        name: 'Bram',
+        age: '21 jaar',
+        place: 'Haarlem',
+        dateIdea: 'Wandeling op het strand bij Wijk aan Zee',
+        category: 'romantisch'
   },
-
-  {
-    name: 'Koen',
-    age: '25 jaar',
-    place: 'Utrecht',
-    dateIdea: 'Skydiven in Zoetermeer, daarna ergens iets drinken',
-    category: 'avontuur'
+    {
+        name: 'Koen',
+        age: '25 jaar',
+        place: 'Utrecht',
+        dateIdea: 'Skydiven in Zoetermeer, daarna ergens iets drinken',
+        category: 'avontuur'
   }
 ]
-
-
-
-
-
-function all(req,res){
-    console.log("hello")
-    res.write('This a datingsite')
-    res.end()
-
-}
