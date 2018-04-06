4adc0f021cb1d2295ce63d70100cdbc6231e0d6a
sterrevangeest <sterrevangeest@hotmail.nl>
2018-04-06 20:37:00 +0200

    data in page matches.ejs is now loaded from mongo database

--- index.js
+++ index.js
@@ -3,8 +3,23 @@
 var express = require('express')
 var mongo = require('mongodb')
 
-var db = null
-var url = 'mongodb://' + process.env.DB_HOST + ':'
+require('dotenv').config
+
+
+var db;
+//var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT //store localhost:27017 from .env in url
+var url = 'mongodb://localhost:27017/'
+
+
+// code: https://github.com/cmda-be/course-17-18/tree/master/examples/mongodb-server by Titus Wormer
+mongo.MongoClient.connect(url, function(err,client){
+  if (err) {
+    throw err
+  }
+  db = client.db('mydatingsite')
+  console.log("YEAH LETZ GO")
+})
+  //end
 
 express()
     .set('view engine', 'ejs')
@@ -17,13 +32,34 @@ express()
     .listen(8000)
 
 
-function all(req, res) {
-    console.log("hello")
-    res.render('matches.ejs', {
-        title: 'matches',
+// function all(req, res, data) {
+//   var database = db.collection('profile').find()
+//   var data = database.toArray()
+//   console.log('matches')
+//   console.log(data)
+//   res.render('matches.ejs', {
+//           data: data
+//         })
+// }
+
+
+function all(req, res, next) {
+  db.collection('profile').find().toArray(done)
+
+  function done(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('matches')
+      console.log(data)
+      res.render('matches.ejs', {
         data: data
-    })
+      })
+    }
+  }
 }
+
+
 
 function detail(req, res) {
 
@@ -32,11 +68,12 @@ function detail(req, res) {
     var length = data.length
     console.log(length)
 
+
     if (id < length) {
         res.statuscode = 200 // 200 OK
         res.render('matches-detail.ejs', {
             title: 'matches-in-detail',
-            data: data[id]
+            data: data
         })
     } else {
       res.statuscode = 404 // 404 Not Found
@@ -52,7 +89,7 @@ function inbox(req, res) {
     console.log("inbox")
     res.render('inbox.ejs', {
         title: 'inbox',
-        data: data
+        //data: data
     })
 }
 
@@ -60,6 +97,6 @@ function profile(req, res) {
     console.log("profile")
     res.render('profile.ejs', {
         title: 'profile',
-        data: data
+        //data: data
     })
 }
